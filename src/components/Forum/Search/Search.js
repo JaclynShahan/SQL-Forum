f4f39f243371b7c04bd860d78171543ec0cb455a
Jaclyn <jaclyndshahan@gmail.com>
2019-12-11 20:22:45 -0600

    workin on search array and reducer

---
+++ src/components/Forum/Search/Search.js
@@ -0,0 +1,67 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {Input} from 'antd';
+
+class Search extends Component {
+    constructor() {
+        super()
+        this.state = {
+            searchStr = ""
+        }
+    }
+    
+    onSearch = () => {
+        if (this.state.searchStr.length > 0) {
+          const filterPost = this.props.post.selectedPost.filter(post => {
+            if (
+              art.title
+                .toLowerCase()
+                .includes(this.state.searchStr.toLowerCase()) ||
+              art.description
+                .toLowerCase()
+                .includes(this.state.searchStr.toLowerCase()) ||
+              art.size.toLowerCase().includes(this.state.searchStr.toLowerCase()) ||
+              art.price.toLowerCase().includes(this.state.searchStr.toLowerCase())
+            ) {
+              return true
+            } else {
+              return false
+            }
+          })
+          console.log('Filtered Post', filterPost)
+          this.props.setSearchPost(filterPost)
+        } else {
+          this.props.setSearchPost([])
+        }
+      }
+
+    render() {
+        const {searchStr} = this.state
+        return (
+            <div>
+            <Input.Search
+            placeholder="Search Posts..."
+            value={searchStr}
+            onChange={e => this.setState({searchStr: e.target.value})}
+            onSearch={() => this.onSearch()}
+            />
+            </div>
+        )
+    }
+}
+
+
+const mapStateToProps = state => {
+   console.log("state:", state)
+    return state
+}
+
+const mapDispatchToProps = dispatch = ({
+    setSearchPost (arr) {
+        dispatch({
+            type: 'SEARCH_POSTS',
+            payload: arr
+        })
+    }
+})
+export default connect(mapStateToProps, mapDispatchToProps)(Search);
---
+++ src/reducers/post.js
@@ -0,0 +1,23 @@
+const initialState = {
+    searchPosts: [],
+   selectedPost: {},
+   newPost: []
+}
+
+export default function reducer (state = initialState, action) {
+    switch (action.type) {
+        case 'SET_POST':
+      return { ...state, user: action.payload }
+    case 'ADD_POST':
+      return { ...state, newPost: action.payload }
+
+    case 'ADD_COMMENT':
+      return { ...state, newComment: action.payload }
+    case 'SELECT_POST':
+      return { ...state, selectedPost: action.payload }
+    case 'SEARCH_POSTS':
+      return { ...state, searchPosts: action.payload }
+  
+    }
+    return state
+}
