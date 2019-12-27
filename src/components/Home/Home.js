006b8757494c3ab27e791f34f66e1bb260c7d837
Jaclyn <jaclyndshahan@gmail.com>
2019-12-26 20:32:12 -0600

    adding buttons for about me and contact section

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import axios from 'axios'
 import { connect } from 'react-redux'
 import './Home.css'
+import {Button} from 'antd'
 
 class Home extends Component {
   constructor () {
@@ -14,6 +15,10 @@ class Home extends Component {
   
         <div className="bodyImage">
             <header className="headerFont">**~WELCOME~**</header>
+            <section className="buttonSection">
+                <button className="buttonSize aboutButton">Learn More</button>
+                <button className="buttonSize">Contact Us</button>
+            </section>
         </div>
        
      
